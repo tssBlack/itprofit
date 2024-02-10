f5ef35c60942dcd530182ab105c0ab4b776c1db0
Nikita Takoy <niktatakoy2607@gmail.com>
2024-02-10 20:21:51 +0300

    last

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,6 +12,10 @@ export function Modal() {
         <h1>
             Благодарю за предоставленную возможность 
         </h1>
+        <div className='social-media'>
+        <a href='https://github.com/tssBlack'>GitHub</a>
+        <a href='https://www.linkedin.com/in/takich/'>LinkedIn</a>
+        </div>
         <button
         className="black-btn" onClick={popupWindow}>Закрыть окно</button>
       </div>
